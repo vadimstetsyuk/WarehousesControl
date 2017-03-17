e51e802e303a096123b7915e00c28598b3bdf8a9
Вадим Стецюк < >
2017-03-18 01:08:04 +0200

    Create User model

---
+++ src/app/auth/User.ts
@@ -0,0 +1,10 @@
+export class User {
+    id: Number;
+    firstName: String;
+    lastName: String;
+    username: String;
+    email: String;
+    password: String;
+    role: String;
+    dateRegister: String;
+}
